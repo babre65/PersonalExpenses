2be753ef081d13c502573e65668a209816b8e890
Reihaneh <rd.roz.dr@gmail.com>
2022-09-08 17:14:49 +0200

    Create BaseResource #5 done.

---
+++ src/main/java/com/reihaneh/personalexpenses/web/rest/BaseResource.java
@@ -0,0 +1,54 @@
+package com.reihaneh.personalexpenses.web.rest;
+
+import com.reihaneh.personalexpenses.domain.BaseEntity;
+import com.reihaneh.personalexpenses.service.BaseService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+public abstract class BaseResource<T extends BaseEntity, S extends BaseService<T, ID>, ID> {
+
+    private final S service;
+
+    public BaseResource(S service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<T> create(@RequestBody T entity) {
+        if (entity.getId() != null)
+            return ResponseEntity.badRequest().build();
+
+        T save = service.save(entity);
+        return ResponseEntity.ok().body(save);
+    }
+
+
+    @PutMapping
+    public ResponseEntity<T> update(@RequestBody T entity) {
+        if (entity.getId() == null)
+            return ResponseEntity.badRequest().build();
+
+        T save = service.save(entity);
+        return ResponseEntity.ok().body(save);
+    }
+
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<T> findById(@PathVariable ID id) {
+
+        Optional<T> result = service.findById(id);
+        return result.map(t -> ResponseEntity.ok().body(t)).orElseGet(() -> ResponseEntity.notFound().build());
+
+    }
+
+    @GetMapping
+    public ResponseEntity<List<T>> findAll() {
+
+        List<T> all = service.findAll();
+        return ResponseEntity.ok().body(all);
+    }
+
+}
