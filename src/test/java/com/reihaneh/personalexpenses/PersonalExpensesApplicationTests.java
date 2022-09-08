13139abf88bf3bca8ec93ea98c0171d7cfe0b19e
Reihaneh <rd.roz.dr@gmail.com>
2022-09-08 14:11:51 +0200

    Init project

---
+++ src/test/java/com/reihaneh/personalexpenses/PersonalExpensesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reihaneh.personalexpenses;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalExpensesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
